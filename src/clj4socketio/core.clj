711059341e93de08c18555f360cc636db9ea86d3
otbzi <ottbzi@gmail.com>
2014-05-05 17:06:36 +0800

    first ci

---
+++ src/clj4socketio/core.clj
@@ -0,0 +1,6 @@
+(ns clj4socketio.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/clj4socketio/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj4socketio.core-test
+  (:require [clojure.test :refer :all]
+            [clj4socketio.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
